0684485b1ee1c9652ecbdb33506efd8b8ea69f6b
LenniM <68513575+LenniM@users.noreply.github.com>
2022-02-22 14:11:13 +0100

    Add files via upload

---
+++ android/app/src/main/java/com/lmdigitaltech/spookypuzzle/MainActivity.java
@@ -0,0 +1,6 @@
+package com.lmdigitaltech.spookypuzzle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/lmdigitaltech/spooky_puzzle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lmdigitaltech.spooky_puzzle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
